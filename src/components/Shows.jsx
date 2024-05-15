b0792a98e590be7f42c7510c8f09ce22218455c9
lokeshdhote <lokeshdhote2004@gmail.com>
2024-05-15 18:15:49 +0530

    first commit

--- src/components/Shows.jsx
+++ src/components/Shows.jsx
@@ -8,13 +8,7 @@ const Shows= ({tasks,settasks})=>{
        settasks(updatedDeleteTasks)
        localStorage.setItem("tasks",JSON.stringify(updatedDeleteTasks))
   }
-  const complete = (index)=>{
-    const copytasks = [...tasks]
-    copytasks[index].completed = !copytasks[index].completed
-    settasks(copytasks)
-    
-    localStorage.setItem("tasks",JSON.stringify(copytasks))
-      }
+
 
     return(
         <>
@@ -23,9 +17,12 @@ const Shows= ({tasks,settasks})=>{
       return(
         <li  key={tasks.id}  className=" mt-[2vw] mb-5 flex justify-left gap-[9vw] items-center border rounded-xl p-5" >
         <div className='flex items-center gap-4 pl-[1vw]'  >
-          <div onClick={()=>complete(index)} 
-          className={ `${tasks.completed ? "bg-green-600" :"border" } w-7 h-7 rounded-full  border-yellow-200 cursor-pointer`} ></div>
-          <h2 className={`${tasks.completed ? "line-through" : ""} capitalize text-white text-2xl font-[400]`} >{tasks.title}</h2>
+
+          <h2 className={`capitalize text-white text-2xl font-[400]`} >{tasks.title}</h2>
+          <h2 className={`capitalize text-white text-2xl font-[400]`} >{tasks.email}</h2>
+
+          <h2 className={`capitalize text-white text-2xl font-[400]`} >{tasks.contact}</h2>
+
         </div>
         <div className=' flex items-center gap-4 pr-[1vw]'>
         <i   className="ri-file-edit-line text-white text-2xl cursor-pointer "></i>
